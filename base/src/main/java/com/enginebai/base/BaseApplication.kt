32f8472d3eb39424619e8f4ab9f9eb7ccbe79180
Engine Bai <enginebai@gmail.com>
2020-11-28 22:14:40 +0800

    Remove some dependency injection modules and MainActivity to let developer to configure by themselves

--- base/src/main/java/com/enginebai/base/BaseApplication.kt
+++ base/src/main/java/com/enginebai/base/BaseApplication.kt
@@ -1,10 +1,6 @@
 package com.enginebai.base
 
 import android.app.Application
-import com.enginebai.base.di.errorHandleModule
-import com.enginebai.base.di.gsonModule
-import com.enginebai.base.di.loggingModule
-import com.enginebai.base.di.networkModule
 import com.orhanobut.logger.AndroidLogAdapter
 import com.orhanobut.logger.FormatStrategy
 import com.orhanobut.logger.Logger
@@ -31,11 +27,7 @@ abstract class BaseApplication : Application() {
         startKoin {
             androidLogger(level = Level.INFO)
             androidContext(this@BaseApplication)
-            val dependencies =
-                mutableListOf(gsonModule, errorHandleModule, networkModule, loggingModule).apply {
-                    addAll(defineDependencies())
-                }
-            modules(dependencies)
+            modules(defineDependencies())
         }
     }
 
--- base/src/main/java/com/enginebai/base/view/BaseActivity.kt
+++ base/src/main/java/com/enginebai/base/view/BaseActivity.kt
@@ -3,18 +3,12 @@ package com.enginebai.base.view
 import android.os.Bundle
 import androidx.annotation.LayoutRes
 import androidx.appcompat.app.AppCompatActivity
-import com.enginebai.base.utils.RxErrorHandler
-import io.reactivex.android.schedulers.AndroidSchedulers
 import io.reactivex.disposables.CompositeDisposable
 import io.reactivex.disposables.Disposable
-import org.koin.android.ext.android.inject
-import java.util.concurrent.TimeUnit
 
 abstract class BaseActivity : AppCompatActivity() {
 
     private val disposables = CompositeDisposable()
-    private val rxErrorHandler: RxErrorHandler by inject()
-    private var rxErrorDisposable: Disposable? = null
 
     @LayoutRes
     abstract fun getLayoutId(): Int
@@ -25,25 +19,6 @@ abstract class BaseActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(getLayoutId())
     }
-
-    override fun onStart() {
-        super.onStart()
-        if (null == rxErrorDisposable || false == rxErrorDisposable?.isDisposed) {
-            rxErrorDisposable = rxErrorHandler.errorMessageToDisplay
-                .filter { it.isNotBlank() }
-                .throttleFirst(2, TimeUnit.SECONDS)
-                .observeOn(AndroidSchedulers.mainThread())
-                .doOnNext { handleErrorMessage(it) }
-                .subscribe()
-                .disposeOnDestroy()
-        }
-    }
-
-    override fun onStop() {
-        super.onStop()
-        rxErrorDisposable?.dispose()
-    }
-
 
     override fun onDestroy() {
         disposables.clear()
