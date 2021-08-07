8cf919f29af38b5dfa105eb45c6aa03eff925d8e
Engine Bai <enginebai@gmail.com>
2021-08-07 20:49:10 +0800

    Add loading and error message to base view model

--- base/src/main/java/com/enginebai/base/view/BaseViewModel.kt
+++ base/src/main/java/com/enginebai/base/view/BaseViewModel.kt
@@ -3,10 +3,14 @@ package com.enginebai.base.view
 import androidx.lifecycle.ViewModel
 import io.reactivex.disposables.CompositeDisposable
 import io.reactivex.disposables.Disposable
+import io.reactivex.subjects.PublishSubject
 import org.koin.core.KoinComponent
 
 abstract class BaseViewModel : ViewModel(), KoinComponent {
     private val disposables = CompositeDisposable()
+
+    val loading by lazy { PublishSubject.create<Boolean>() }
+    val errorMessage by lazy { PublishSubject.create<String>() }
 
     override fun onCleared() {
         disposables.dispose()
--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -19,7 +19,7 @@ plugins {
 
 dependencies {
     // Depend on the android gradle plugin, since we want to access it in our plugin
-    implementation("com.android.tools.build:gradle:4.0.0")
+    implementation("com.android.tools.build:gradle:4.2.2")
 
     // Depend on the kotlin plugin, since we want to access it in our plugin
     implementation("org.jetbrains.kotlin:kotlin-gradle-plugin:1.3.72")
